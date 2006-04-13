22a6b0e1afd845610bee94f75ee81bd58cc4dd82
timf <timf@bdd19a93-b610-0410-a796-f21600580ca4>
2006-04-13 16:41:06 +0000

    Tagging the 1.0.10 release of EVEMon
    git-svn-id: file:///home/trf/home/svnuser/repo@72 bdd19a93-b610-0410-a796-f21600580ca4

---
+++ evemon/tags/release-1.0.10/AboutWindow.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Reflection;
+
+namespace EveCharacterMonitor
+{
+    public partial class AboutWindow : Form
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutWindow_Load(object sender, EventArgs e)
+        {
+            Version currentVersion = new Version("0.0.0.0");
+            foreach (Attribute a in Assembly.GetExecutingAssembly().GetCustomAttributes(false))
+            {
+                if (a is AssemblyFileVersionAttribute)
+                {
+                    AssemblyFileVersionAttribute ava = a as AssemblyFileVersionAttribute;
+                    currentVersion = new Version(ava.Version);
+                }
+            }
+
+            lblVersion.Text = String.Format(lblVersion.Text, currentVersion.ToString());
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
