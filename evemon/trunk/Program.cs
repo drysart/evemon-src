ec238f4d7f830fdbf32e8f21b7caaab37348323a
timf <timf@bdd19a93-b610-0410-a796-f21600580ca4>
2006-04-14 23:05:28 +0000

    Better unhandled exception handling. All untrapped exceptions should be caught now.
    git-svn-id: file:///home/trf/home/svnuser/repo@90 bdd19a93-b610-0410-a796-f21600580ca4

--- evemon/trunk/Program.cs
+++ evemon/trunk/Program.cs
@@ -13,24 +13,37 @@ namespace EveCharacterMonitor
         [STAThread]
         static void Main()
         {
+#if !DEBUG
             InstanceManager im = InstanceManager.GetInstance();
             if (!im.CreatedNew)
             {
                 im.Signal();
                 return;
             }
+#endif
 
             m_settingKey = String.Empty;
 
             EveSession.MainThread = System.Threading.Thread.CurrentThread;
 
             Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1(ca));
             s_settings = Settings.LoadFromKey(m_settingKey);
             Application.Run(new MainWindow(s_settings));
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                ShowError((Exception)e.ExceptionObject);
+                Application.Exit();
+            }
+            catch { }
         }
 
         public static string SettingKey
@@ -42,16 +55,25 @@ namespace EveCharacterMonitor
         private static Settings s_settings;
         private static bool m_showWindowOnError = true;
 
-        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            ShowError(e.Exception);
+            try
+            {
+                Application.Exit();
+            }
+            catch { }
+        }
+
+        private static void ShowError(Exception e)
         {
             if (m_showWindowOnError)
             {
                 m_showWindowOnError = false;
-                using (UnhandledExceptionWindow f = new UnhandledExceptionWindow(e.Exception))
+                using (UnhandledExceptionWindow f = new UnhandledExceptionWindow(e))
                 {
                     f.ShowDialog();
                 }
-                Application.Exit();
             }
         }
     }
