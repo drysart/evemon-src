7af7ccb63f8366fd53b776ae29b7c4915b67d87b
timf <timf@bdd19a93-b610-0410-a796-f21600580ca4>
2006-04-26 09:18:04 +0000

    Tagging the 1.0.20.1 release of EVEMon
    git-svn-id: file:///home/trf/home/svnuser/repo@185 bdd19a93-b610-0410-a796-f21600580ca4

---
+++ evemon/tags/release-1.0.20.1/EVEMonInstallBuilder/Program.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace EVEMonInstallBuilder
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+#if !DEBUG
+                string config = "Release";
+#endif
+#if DEBUG
+                string config = "Debug";
+                return 0;
+#endif
+                string projectDir = String.Join(" ", args);
+                string desktopDir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+                Assembly exeAsm = Assembly.LoadFrom("../../../bin/"+config+"/EVEMon.exe");
+                string ver = String.Empty;
+                foreach (Attribute a in exeAsm.GetCustomAttributes(false))
+                {
+                    if (a is AssemblyFileVersionAttribute)
+                    {
+                        AssemblyFileVersionAttribute ava = a as AssemblyFileVersionAttribute;
+                        ver = ava.Version;
+                    }
+                }
+                if (String.IsNullOrEmpty(ver))
+                    throw new ApplicationException("no version");
+
+                string param =
+                    "/DVERSION=" + ver + " " +
+                    "\"/DOUTDIR=" + desktopDir + "\" " +
+                    //"/PAUSE "+
+                    "\"EVEMon Installer Script.nsi\"";
+                //System.Windows.Forms.MessageBox.Show(param);
+                System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo(
+                    "C:/Program Files/NSIS/makensis.exe", param);
+                psi.WorkingDirectory = projectDir;
+                System.Diagnostics.Process makensisProcess = System.Diagnostics.Process.Start(psi);
+                makensisProcess.WaitForExit();
+                int exitCode = makensisProcess.ExitCode;
+                makensisProcess.Dispose();
+
+                return exitCode;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+                return 1;
+            }
+        }
+    }
+}
