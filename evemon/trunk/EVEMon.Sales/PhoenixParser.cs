6efd267433a60e4c968ae0dfe008dc42946c3da4
anders <anders@bdd19a93-b610-0410-a796-f21600580ca4>
2006-05-20 05:51:08 +0000

    trying to fix the foreign currency parse problem in the mineral module
    git-svn-id: file:///home/trf/home/svnuser/repo@273 bdd19a93-b610-0410-a796-f21600580ca4

--- evemon/trunk/EVEMon.Sales/PhoenixParser.cs
+++ evemon/trunk/EVEMon.Sales/PhoenixParser.cs
@@ -63,7 +63,7 @@ namespace EVEMon.Sales
             foreach (Match mineral in mc)
             {
                 string name = mineral.Groups["name"].Value;
-                price = Decimal.Parse(mineral.Groups["price"].Value, System.Globalization.NumberStyles.Currency);
+                Decimal price = Decimal.Parse(mineral.Groups["price"].Value, System.Globalization.NumberStyles.Currency);
                 yield return new Pair<string, Decimal>(name, price);
             }
         }
